90a8588c964b2f191f9243ea9a7227b576722736
cjy0019 <cjy0029@naver.com>
2020-12-23 18:04:47 +0900

    conf : configuration

---
+++ src/components/Signin.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Row, Col } from 'antd';
+
+const Signin = (props) => (
+  <Row align='middle'>
+    <Col span={24}></Col>
+  </Row>
+);
+
+export default Signin;
---
+++ src/pages/Signin.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Signin from '../components/Signin';
+
+export default function SinginPage() {
+  return <Signin />;
+}
