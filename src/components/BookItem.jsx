4038c5b2a96743f794b39b8f70be39e0d013fd4b
cjy0019 <cjy0029@naver.com>
2020-12-27 19:33:15 +0900

    docs : styling

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -1,19 +1,86 @@
-import { message } from 'antd';
 import React from 'react';
 import { Button } from 'antd';
-import { HomeOutlined } from '@ant-design/icons';
+import {
+  RightCircleOutlined,
+  EditOutlined,
+  DeleteOutlined,
+} from '@ant-design/icons';
+import styled from 'styled-components';
+import PostImg from '../img/PostitFinal.png';
+
+const BookDiv = styled.div`
+  background-image: url(${PostImg});
+  background-position-y: 50%;
+  background-position-x: 55%;
+  background-repeat: no-repeat;
+  background-size: 140% 140%;
+  z-index: 100;
+  width: 60vw;
+  height: 50vh;
+  padding: 40px;
+  margin-top: 30px;
+  /* border: 3px solid black; */
+`;
+
+const StyleP = styled.p`
+  word-wrap: break-all;
+  max-height: 11vh;
+  overflow: hidden;
+  word-break: keep-all;
+  /* text-overflow: ellipsis; */
+  padding-bottom: 20px;
+  font-size: 13px;
+`;
+
+const Styleh2 = styled.h2`
+  font-size: 25px;
+  word-break: keep-all;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 20%;
+`;
+
+const Flexbox = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  line-height: 10px;
+  vertical-align: top;
+`;
+
+const ButtonStyle = {
+  backgroundColor: 'transparent',
+  border: 'none',
+  boxShadow: '0 0 0 0',
+};
+
+const TextDiv = styled.div`
+  text-align: center;
+  height: 3vh;
+  font-size: 11px;
+  text-align: center;
+  padding: 0 10px;
+`;
 
 export default function BookItem({ title, author, message, url }) {
   return (
-    <div>
-      <h2>
-        {title}
+    <BookDiv>
+      <Styleh2>{title}</Styleh2>
+      <Flexbox>
         <a href={url} target='_BLANK' rel='noreferrer'>
-          <Button icon={<HomeOutlined />} />
+          <Button style={ButtonStyle} icon={<RightCircleOutlined />} />
         </a>
-      </h2>
-      <h3>{author}</h3>
-      <p>{message}</p>
-    </div>
+        <a href={url} target='_BLANK' rel='noreferrer'>
+          <Button style={ButtonStyle} icon={<EditOutlined />} />
+        </a>
+        <a href={url} target='_BLANK' rel='noreferrer'>
+          <Button style={ButtonStyle} icon={<DeleteOutlined />} />
+        </a>
+      </Flexbox>
+      <TextDiv>
+        <h3 style={{ fontSize: '20px' }}>by {author}</h3>
+        <StyleP>{message}</StyleP>
+      </TextDiv>
+    </BookDiv>
   );
 }
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,9 +1,86 @@
 import axios from 'axios';
 import React, { Component } from 'react';
 import { sleep } from '../utils';
-import { LoadingOutlined, ReloadOutlined } from '@ant-design/icons';
-import { Button } from 'antd';
+import {
+  LoadingOutlined,
+  ReloadOutlined,
+  PoweroffOutlined,
+  BookOutlined,
+  AudioOutlined,
+} from '@ant-design/icons';
+import { Button, Input } from 'antd';
 import BookItem from './BookItem';
+import styled from 'styled-components';
+import img from '../img/hohobook.png';
+const { Search } = Input;
+// 스타일
+
+const MyContainer = styled.div`
+  padding: 30px 70px;
+`;
+
+const MyHeader = styled.header`
+  display: flex;
+  width: 100%;
+  margin-top: 50px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
+  /* z-index: 100; */
+`;
+
+const StyleButton = {
+  marginRight: '15px',
+  fontSize: '18px',
+  backgroundColor: '#FF8B8B',
+  border: '1px solid #FF8B8B',
+  color: '#ffffff',
+  borderRadius: '5px',
+  marginTop: '3vh',
+};
+
+const Logout = {
+  backgroundColor: '#fdf9da',
+  fontSize: '18px',
+  border: '1px solid #F9F7E8',
+  color: '#61bfad',
+  marginRight: '30px',
+  borderRadius: '5px',
+};
+
+const SectionVisual = styled.section`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-image: url(${img});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position-y: 65%;
+  width: 100%;
+  height: 45vh;
+  /* z-index: -100; */
+
+  &::before {
+    content: '';
+    z-index: -50;
+    background-color: black;
+    opacity: 0.3;
+    display: block;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+  }
+`;
+
+const FlexContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 export default class BookList extends Component {
   state = {
@@ -34,14 +111,49 @@ export default class BookList extends Component {
     }
 
     return (
-      <div>
-        <h1>Book List{loading && <LoadingOutlined />}</h1>
-        {books.length === 0 && <p>데이터가 없습니다.</p>}
-        {books.length !== 0 &&
-          books.map((book) => {
-            return <BookItem {...book} />;
-          })}
-      </div>
+      <MyContainer>
+        <SectionVisual>
+          <MyHeader>
+            <div>
+              <Button
+                type='primary'
+                icon={<BookOutlined />}
+                style={StyleButton}>
+                Add books
+              </Button>
+              <Button
+                type='primary'
+                icon={<PoweroffOutlined />}
+                loading={0}
+                style={Logout}>
+                Logout
+              </Button>
+            </div>
+          </MyHeader>
+          <h3
+            style={{ marginBottom: ' 1vh', fontSize: '60px', color: 'white' }}>
+            BOOK STUDIO
+          </h3>
+
+          <Search
+            placeholder='지금 떠오르는 책을 찾아보세요...'
+            allowClear
+            size='large'
+            style={{
+              margin: '0 10px',
+              width: '35vw',
+              marginBottom: '28vh',
+            }}
+          />
+        </SectionVisual>
+        <FlexContainer>
+          {books.length === 0 && <p>데이터가 없습니다.</p>}
+          {books.length !== 0 &&
+            books.map((book, index) => {
+              return <BookItem key={index} {...book} />;
+            })}
+        </FlexContainer>
+      </MyContainer>
     );
   }
 
